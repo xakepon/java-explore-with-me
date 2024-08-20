d24aa387ba158850b48b58e5ff0ccba00144151d
Marat <xakeponus@yandex.ru>
2024-08-20 22:32:48 +0300

    v0.5

--- ewm-service/src/main/java/ru/practicum/persistence/repository/EventRep.java
+++ ewm-service/src/main/java/ru/practicum/persistence/repository/EventRep.java
@@ -29,8 +29,8 @@ public interface EventRep  extends JpaRepository<Event, Long> {
     @Query(value = "SELECT * " +
             "FROM event e " +
             "WHERE (:userId IS NULL OR e.initiator_id IN (CAST(CAST(:userId AS TEXT) AS BIGINT))) " +
-            "AND (:states IS NULL OR e.state IN (CAST(:states AS TEXT))) " +
-            "AND (:categories IS NULL OR e.category_id IN (CAST(CAST(:categories AS TEXT) AS BIGINT))) " +
+            "AND (CAST(:states AS TEXT) IS NULL OR e.state IN (CAST(:states AS TEXT))) " +
+            "AND (CAST(:categories AS TEXT) IS NULL OR e.category_id IN (CAST(CAST(:categories AS TEXT) AS BIGINT))) " +
             "AND (CAST(:rangeStart AS TIMESTAMP) IS NULL OR e.event_date >= CAST(:rangeStart AS TIMESTAMP))" +
             "AND (CAST(:rangeEnd AS TIMESTAMP) IS NULL OR e.event_date < CAST(:rangeEnd AS TIMESTAMP))",
             nativeQuery = true)
