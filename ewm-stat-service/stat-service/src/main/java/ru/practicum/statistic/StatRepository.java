80629bd5c00f4d03f308c7715e191eff79676339
Marat <xakeponus@yandex.ru>
2024-08-21 00:24:26 +0300

    v0.6

--- ewm-stat-service/stat-service/src/main/java/ru/practicum/statistic/StatRepository.java
+++ ewm-stat-service/stat-service/src/main/java/ru/practicum/statistic/StatRepository.java
@@ -15,7 +15,7 @@ public interface StatRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.api.ViewStat(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp BETWEEN :start AND :end " +
-            "AND :uris IS NULL  OR eh.uri IN :uris) " +
+            "AND (COALESCE(:uris, NULL) IS NULL OR eh.uri IN :uris) " +
             "GROUP BY eh.app, eh.uri " +
             "ORDER BY COUNT(DISTINCT eh.ip) DESC")
     List<ViewStat> findAllUniqueViewStatList(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
@@ -24,7 +24,7 @@ public interface StatRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.api.ViewStat(eh.app, eh.uri, COUNT(eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp BETWEEN :start AND :end " +
-            "AND :uris IS NULL OR eh.uri IN :uris) " +
+            "AND (COALESCE(:uris, NULL) IS NULL OR eh.uri IN :uris) " +
             "GROUP BY eh.app, eh.uri " +
             "ORDER BY COUNT(eh.ip) DESC")
     List<ViewStat> findAllViewStatList(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
