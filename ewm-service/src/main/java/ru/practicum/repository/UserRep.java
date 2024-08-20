7a4e18a539761d95776717985eacc37cd62bbe6d
Marat <xakeponus@yandex.ru>
2024-08-20 12:36:00 +0300

    v0.4

--- ewm-service/src/main/java/ru/practicum/repository/UserRep.java
+++ ewm-service/src/main/java/ru/practicum/repository/UserRep.java
@@ -2,10 +2,12 @@ package ru.practicum.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.models.User;
 
 import java.util.List;
 
+@Repository
 public interface UserRep extends JpaRepository<User, Long> {
 
     List<User> findAllByIdIn(List<Long> ids, Pageable page);
